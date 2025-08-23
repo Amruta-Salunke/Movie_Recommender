64fcb96ee52372909499187408ceda8bd78108da
Amruta Salunkhe <amruta.n.salunkhe99@gmail.com>
2025-08-23 17:19:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ import requests
 
 movies = pickle.load(open('moives_list.pkl','rb'))
 
-st.header("Movie Recommender System")
-st.selectbox("Select movie from dropdown",movies)
+movies_list=movies['title'].values
 
-if st.button("Show recommend"):
+
+st.header("Movie Recommender System")
+st.selectbox("Select movie from dropdown",movies_list)
+
+
+if st.button("Show Recommend"):
      pass
